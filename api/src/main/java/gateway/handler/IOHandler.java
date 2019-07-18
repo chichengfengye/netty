ce2eeb3c7ea84e43f6e43444b2dc5298e89e470a
chichengfengye <1639700823@qq.com>
2019-07-18 10:27:14 +0800

    转发请求与响应的时候都把header无差别转发

--- api/src/main/java/gateway/handler/IOHandler.java
+++ api/src/main/java/gateway/handler/IOHandler.java
@@ -6,7 +6,6 @@ import gateway.common.ReturnResult;
 import gateway.common.RouteInfo;
 import gateway.handler.ab.AbstractInBoundAdapterHandler;
 import gateway.helper.ResponseBackHelper;
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
@@ -21,19 +20,13 @@ public class IOHandler extends AbstractInBoundAdapterHandler {
 
     @Override
     protected ReturnResult businessRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf byteBuf = null;
+        HttpResponse response = null;
         //接受數據
         RouteInfo routeInfo = (RouteInfo) ctx.channel().attr(AttributeKey.valueOf(Constant.ROUTEI_INFO)).get();
         if (routeInfo != null) {
             // 進行轉發
-            String result = null;
             try {
-//                result = ResponseBackHelper.sendRequest(routeInfo.getHttpMethod(), routeInfo.getUri(), (FullHttpRequest) msg);
-                HttpResponse response = ResponseBackHelper.sendRequest2(routeInfo.getHttpMethod(), routeInfo.getUri(), (FullHttpRequest) msg);
-                if (response != null) {
-                    byteBuf = Unpooled.copiedBuffer(result, CharsetUtil.UTF_8);
-                }
-
+                response = ResponseBackHelper.sendRequest2(routeInfo.getHttpMethod(), routeInfo.getUri(), (FullHttpRequest) msg);
             } catch (Exception e) {
                 e.printStackTrace();
                 ReturnResult.failure(NettyCode.COMMON_EXCEPTION, "SERVER INTERNAL EXCEPTION");
@@ -44,9 +37,13 @@ public class IOHandler extends AbstractInBoundAdapterHandler {
         //把结果返回给客户端
         //1. 直接返回数据内容包括数据类型等属性信息
         //2. 如果只能自己设定返回数据的格式，那就只能自己先分析理数类型，再设置相应头并返回
-        HttpResponse httpResponse= ResponseBackHelper.httpJsonResponse(byteBuf);
-        ctx.writeAndFlush(httpResponse);
+        if (response == null) {
+            response = ResponseBackHelper.httpJsonResponse(Unpooled.copiedBuffer("", CharsetUtil.UTF_8));
+        }
+
+        ctx.writeAndFlush(response);
         ctx.close();
+
         return ReturnResult.success();
     }
 }
--- api/src/main/java/gateway/helper/ResponseBackHelper.java
+++ api/src/main/java/gateway/helper/ResponseBackHelper.java
@@ -9,11 +9,10 @@ import okhttp3.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class ResponseBackHelper {
 
-    public static String sendRequest(HttpMethod httpMethod, String url, FullHttpRequest request) throws Exception{
+    public static String sendRequest(HttpMethod httpMethod, String url, FullHttpRequest request) throws Exception {
         String result = "just support GET|POST method";
         OkHttpClient client = new OkHttpClient();
         Request okRequest = null;
@@ -55,13 +54,11 @@ public class ResponseBackHelper {
         return result;
     }
 
-    public static HttpResponse sendRequest2(HttpMethod httpMethod, String url, FullHttpRequest request) throws Exception{
-        HttpResponse response = null;
-        String result = "just support GET|POST method";
+    public static HttpResponse sendRequest2(HttpMethod httpMethod, String url, FullHttpRequest request) throws Exception {
+        HttpResponse response;
+        Request okRequest;
+
         OkHttpClient client = new OkHttpClient();
-        Request okRequest = null;
-        RequestBody body;
-        String reqBody = "";
 
         if (httpMethod.equals(HttpMethod.GET)) {
             okRequest = new Request.Builder()
@@ -69,24 +66,30 @@ public class ResponseBackHelper {
 //                        .get(null)
                     .build();
         } else if (httpMethod.equals(HttpMethod.POST)) {
-            HttpHeaders headers = request.headers();
-            MediaType mediaType;
+            HttpHeaders reqHeaders = request.headers();
             ByteBuf byteContent = request.content();
-            if (byteContent.isReadable()) {
-                reqBody = byteContent.toString(CharsetUtil.UTF_8);
+            String reqBody = byteContent.isReadable() ? byteContent.toString(CharsetUtil.UTF_8) : "";
+
+            String[] nameAndValues = new String[reqHeaders.size() * 2];
+            int i = 0;
+            for (Map.Entry<String, String> reqHeader : reqHeaders) {
+                nameAndValues[i] = reqHeader.getKey();
+                nameAndValues[i + 1] = reqHeader.getValue();
+                i += 2;
             }
 
-            String contentType = headers.get("content-type");
-            mediaType = MediaType.get(contentType + "; charset=utf-8");
+            RequestBody okBody = RequestBody.create(
+                    MediaType.get(reqHeaders.get("content-type") + "; charset=utf-8"),
+                    reqBody);
 
-            body = RequestBody.create(mediaType, reqBody);
             okRequest = new Request.Builder()
                     .url(url)
-                    .post(body)
+                    .headers(Headers.of(nameAndValues))
+                    .post(okBody)
                     .build();
 
         } else {
-            return response;
+            return httpJsonResponse(Unpooled.copiedBuffer("just support GET|POST method", CharsetUtil.UTF_8));
         }
 
         Response okResponse = client.newCall(okRequest).execute();
@@ -95,8 +98,8 @@ public class ResponseBackHelper {
         return response;
     }
 
-    private static HttpResponse convertResponse(Response okResponse) {
-        String contentStr = okResponse.body().toString();
+    private static HttpResponse convertResponse(Response okResponse) throws IOException {
+        String contentStr = okResponse.body().string();
         contentStr = contentStr == null ? "null" : contentStr;
         ByteBuf content = Unpooled.copiedBuffer(contentStr, CharsetUtil.UTF_8);
         HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
@@ -104,22 +107,23 @@ public class ResponseBackHelper {
         if (headers != null) {
             Map<String, List<String>> map = headers.toMultimap();
             for (Map.Entry<String, List<String>> stringListEntry : map.entrySet()) {
+                System.out.println(stringListEntry.getKey() + ":" + stringListEntry.getValue());
                 response.headers().set(stringListEntry.getKey(), stringListEntry.getValue());
             }
         }
-//            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
-//            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
         return response;
     }
 
     /**
      * 返回json格式数据
+     *
      * @param content
      * @return
      */
     public static HttpResponse httpJsonResponse(ByteBuf content) {
         if (content == null) {
-            content = Unpooled.copiedBuffer("null",CharsetUtil.UTF_8);
+            content = Unpooled.copiedBuffer("null", CharsetUtil.UTF_8);
         }
         HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
